41e634c5805ea0eee10c2d0f2e1d16ba2f2abc6f
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-18 16:21:47 -0300

    created: estilos css para el contenido

--- assetts/js/miscript.js
+++ assetts/js/miscript.js
@@ -6,3 +6,10 @@ let inputOne = document.querySelector('#one');
 let inputTwo = document.querySelector('#two');
 let inputThree = document.querySelector('#three');
 let total = document.querySelector('#total');
+
+
+/* parte 3 */
+
+let selectOne = document.querySelector('#selectOne');
+let selectTwo = document.querySelector('#selectTwo');
+let selectThree = document.querySelector('#selectThree');
