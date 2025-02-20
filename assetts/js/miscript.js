4fd16679543ee42f17484e96abd5535dda8433d1
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-20 16:34:55 -0300

    updated: botones de enlace y mensaje de password

--- assetts/js/miscript.js
+++ assetts/js/miscript.js
@@ -1,17 +1,16 @@
 /* parte 1 */
-elementoDog = document.querySelector('#dog');
+elementoDog = document.querySelector("#dog");
 
 /* parte 2 */
-let inputOne = document.querySelector('#one');
-let inputTwo = document.querySelector('#two');
-let inputThree = document.querySelector('#three');
-let total = document.querySelector('#total');
-
+let inputOne = document.querySelector("#one");
+let inputTwo = document.querySelector("#two");
+let inputThree = document.querySelector("#three");
+let total = document.querySelector("#total");
 
 /* parte 3 */
 
-let selectOne = document.querySelector('#selectOne');
-let selectTwo = document.querySelector('#selectTwo');
-let selectThree = document.querySelector('#selectThree');
+let selectOne = document.querySelector("#selectOne");
+let selectTwo = document.querySelector("#selectTwo");
+let selectThree = document.querySelector("#selectThree");
 
-let acceso = document.querySelector('#acceso');
+let acceso = document.querySelector("#acceso");
