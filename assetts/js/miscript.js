16e856aef42415ce2e73e1f548f0ab0a5d5577c5
framirezj <12649259+framirezj@users.noreply.github.com>
2025-02-20 16:22:16 -0300

    created: funcionalidad javascript

--- assetts/js/miscript.js
+++ assetts/js/miscript.js
@@ -13,3 +13,5 @@ let total = document.querySelector('#total');
 let selectOne = document.querySelector('#selectOne');
 let selectTwo = document.querySelector('#selectTwo');
 let selectThree = document.querySelector('#selectThree');
+
+let acceso = document.querySelector('#acceso');
